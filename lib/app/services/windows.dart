c3c52040c84daaa4fbb0fe1401f6a2c650427eec
Anuchit Chalothorn <anoochit@gmail.com>
2024-11-06 09:22:15 +0700

    update init windows size for desktop platform

--- lib/app/services/windows.dart
+++ lib/app/services/windows.dart
@@ -6,8 +6,7 @@ import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 initWindows() {
   // check desktop platform to config windows size
-  // TODO : check another desktop eg: linux, macos
-  if (Platform.isWindows) {
+  if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
     doWhenWindowReady(() {
       const initialSize = Size(480, 853);
       appWindow.size = initialSize;
