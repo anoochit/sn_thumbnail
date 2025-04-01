625ff4799c9927158fe16339cbe7415fe13680d7
Anuchit Chalothorn <anoochit@gmail.com>
2025-04-01 11:56:58 +0700

    update cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,7 @@
+// import { onRequest } from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+import { onCall } from "firebase-functions/v2/https";
+
 /**
  * Import function triggers from their respective submodules:
  *
@@ -7,13 +11,21 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+/*
+Start writing functions
+https://firebase.google.com/docs/functions/typescript
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+*/
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const geminiImageGen = onCall((request, response) => {
+  // Message text passed from the client.
+  const text = request.data.text;
+  // Authentication / user information is automatically added to the request.
+  // const uid = request.auth.uid;
+  logger.info("your text: ", text);
+  return { text: text };
+});
