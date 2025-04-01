40921e8c5dbe594134d890ceba41f29cd3d6f66e
Anuchit Chalothorn <anoochit@gmail.com>
2025-04-01 12:21:44 +0700

    update image gen

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
-// import { onRequest } from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
-import { onCall } from "firebase-functions/v2/https";
+import { onCall, HttpsError } from "firebase-functions/v2/https";
+import { GoogleGenAI } from "@google/genai";
 
 /**
  * Import function triggers from their respective submodules:
@@ -11,21 +12,33 @@ import { onCall } from "firebase-functions/v2/https";
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-/*
-Start writing functions
-https://firebase.google.com/docs/functions/typescript
+ 
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
-*/
 
-export const geminiImageGen = onCall((request, response) => {
-  // Message text passed from the client.
-  const text = request.data.text;
+
+export const geminiImageGen = onCall(async (request) => {
+  // Message content passed from the client.
+  const content = request.data.text;
   // Authentication / user information is automatically added to the request.
-  // const uid = request.auth.uid;
-  logger.info("your text: ", text);
-  return { text: text };
+  // const uid  = request.auth?.uid;
+
+  // const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+  const GEMINI_API_KEY = "AIzaSyCv9ZYJ4s4N5ym1yuLUWMuZdKIgy-GEhEQ";
+  const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+  const result = await ai.models.generateContent({
+    model: "gemini-2.0-flash-exp-image-generation",
+    contents: content,
+    config: {
+      responseModalities: ["Text", "Image"],
+    },
+  });
+
+  logger.info("your prompt: ", content);
+  return { resutl: result };
 });
